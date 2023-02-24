d447363fa3e2abec32686aefda9ba31a4f559c3e
Muhammed Semih <muhammedsemihekmen@hotmail.com>
2023-02-24 22:59:54 +0300

    finished

--- SimpleTextShareApp/ViewController.swift
+++ SimpleTextShareApp/ViewController.swift
@@ -13,16 +13,27 @@ SHARE PROPERTY
  * if you want to check action, go to the message app and just paste it. you will see :)
  
 SWITCH USAGE
- * 
+ * we have added the switch from library on storyboard. Also, we have added the a button for show to how to reach switch from button.
+ 
+PROGRESS BAR WITH TEXTFIELD
+ 
+
 */
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
     @IBOutlet weak var loremLabel: UILabel!
+    @IBOutlet weak var themeSwitch: UISwitch!
+    @IBOutlet weak var exaTextField: UITextField!
+    @IBOutlet weak var exaProgressBar: UIProgressView!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        exaTextField.delegate = self
+        indicator.stopAnimating()
+        indicator.isHidden = true
     }
 
     @IBAction func shareButtonAction(_ sender: UIButton) {
@@ -31,5 +42,34 @@ class ViewController: UIViewController {
         present(activityCtrl, animated: true) // it similar to alert dialog.
     }
     
+    
+    @IBAction func changeTheme(_ sender: UIButton) {
+        themeSwitch.setOn(!themeSwitch.isOn, animated: true)   //setOn function changes the off/on situation.
+        switchAction(themeSwitch)                             // we could use the our switchAction function but it has needed the UISwitch parameter.
+    }
+    
+    @IBAction func switchAction(_ sender: UISwitch) {
+        self.view.backgroundColor = self.view.backgroundColor == .white ? .darkGray : .white  // in here, we have changed the background color.
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let lenght = Float(textField.text?.count ?? 1)/10
+        if lenght < 1 {
+            exaProgressBar.setProgress(lenght, animated: true)
+        }else{
+            exaProgressBar.setProgress(1.0, animated: true)
+        }
+        return true
+    }
+    
+    @IBAction func indicatorProcess(_ sender: UIButton) {
+        if indicator.isAnimating == true {
+            indicator.stopAnimating()
+            indicator.isHidden = true
+        }else{
+            indicator.isHidden = false
+            indicator.startAnimating()
+        }
+    }
 }
 
