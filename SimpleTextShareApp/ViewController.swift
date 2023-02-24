7b5a2471351aa10dcd5967374c81ae2d4bfe2ddb
Muhammed Semih <muhammedsemihekmen@hotmail.com>
2023-02-24 18:09:16 +0300

    Initial Commit

---
+++ SimpleTextShareApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleTextShareApp
+//
+//  Created by Semih Ekmen on 24.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
